5e9306e90792231c6a06522d7665526f60fb6ba7
lmuratore <ltm157m@gmail.com>
2023-02-17 18:57:27 -0500

    First commit - hello world and hello cargo

---
+++ hello-world/main.rs
@@ -0,0 +1,8 @@
+// main function is the first code that runs in every executable rust program
+fn main() {
+    // ! at the end of println signifies that it is a macro and not a normal function
+    // end line with semicolon
+    println!("Hello, world!");
+}
+// to compile, run "rustc main.rs"
+// to run executable, run "./main"
